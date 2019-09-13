ce226b46756a538f553f8977478e0105111c7bce
siijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-09-13 23:45:50 +0900

    メッセージ画面1

---
+++ pages/messege.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message-list-content">
+    <ul class="hidden-area-list-group">
+      <li class="list-entrt" deta-user-id="0">
+        <div class="flex-parent" />
+          <div class="main-photo-circle">
+            <div class="photo-area">
+              <div class="photo-area-inner" />
+            </div>
+          </div>
+          <div class="message-list-info">
+            <div class="profile-info-flex">
+              <p class="info-text">
+                <span>R</span>
+                <span>31才</span>
+                <span>東京></span>
+              </p>
+            </div>
+            <div class="list-message">
+              <p class="message-contents">
+                <span class="message-contents">お疲れ様です！</span>
+              </p>
+            </div>
+            <div class="action-area">
+              <time>17分前</time>
+            </div>
+          </div>
+          <div class="js-dwordown" />
+      </li>
+    </ul>
+  </div>
+
+</template>
